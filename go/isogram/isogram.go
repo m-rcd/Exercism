7eaf23cd1bf7422e91a93b2a9c76f54db3ec635f
Marianne Rachid <marianne.rachid@gmail.com>
2020-04-28 12:26:10 +0100

    isogram

---
+++ go/isogram/isogram.go
@@ -0,0 +1,20 @@
+package isogram
+
+import (
+	"strings"
+)
+
+func IsIsogram(word string) bool {
+	tracker := map[rune]int{}
+
+	FormattedWord := strings.NewReplacer((strings.ToLower(word), "-", "")
+	FormattedWord = strings.ReplaceAll(FormattedWord, " ", "")
+
+	for _, v := range FormattedWord {
+		tracker[v] += 1
+		if tracker[v] > 1 {
+			return false
+		}
+	}
+	return true
+}
