dc198025b3dc3b3c124d71a5d0ea968a9bd50f93
Marianne Rachid <marianne.rachid@gmail.com>
2020-05-02 13:12:42 +0100

    Refactor isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -1,19 +1,19 @@
 package isogram
 
 import (
-	"strings"
+	"unicode"
 )
 
 func IsIsogram(word string) bool {
-	tracker := map[rune]int{}
+	count := map[rune]int{}
 
-	FormattedWord := strings.ReplaceAll(strings.ToLower(word), "-", "")
-	FormattedWord = strings.ReplaceAll(FormattedWord, " ", "")
-
-	for _, v := range FormattedWord {
-		tracker[v] += 1
-		if tracker[v] > 1 {
-			return false
+	for _, letter := range word {
+		letter = unicode.ToLower(letter)
+		if unicode.IsLetter(letter) {
+			count[letter] += 1
+			if count[letter] > 1 {
+				return false
+			}
 		}
 	}
 	return true
