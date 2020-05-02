84a9a334fde49dc92c7775651892aa65121ecbc9
Marianne Rachid <marianne.rachid@gmail.com>
2020-05-02 13:18:24 +0100

    Refactor isogram

--- go/isogram/isogram.go
+++ go/isogram/isogram.go
@@ -8,13 +8,16 @@ func IsIsogram(word string) bool {
 	count := map[rune]int{}
 
 	for _, letter := range word {
-		letter = unicode.ToLower(letter)
-		if unicode.IsLetter(letter) {
-			count[letter] += 1
-			if count[letter] > 1 {
-				return false
-			}
+		if !unicode.IsLetter(letter) {
+			continue
 		}
+
+		letter = unicode.ToLower(letter)
+		count[letter] += 1
+		if count[letter] > 1 {
+			return false
+		}
+
 	}
 	return true
 }
